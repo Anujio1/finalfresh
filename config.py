50d2948b51e29f21b35c8c0bab3bd35cf055b978
Arif Mustafa Khan <98647115+ArifMustafaKhan@users.noreply.github.com>
2025-05-03 20:29:44 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "28485705"
+api_hash = "26d76803d487d6d0cc818cf191fa7518"
+bot_token = "7782951065:AAENXPxkbEX_yInMl2H3iruu1sEeFeAQMYw"
+auth_users = [6211690957]
